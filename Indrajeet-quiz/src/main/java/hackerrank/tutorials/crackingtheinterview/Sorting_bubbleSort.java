580f609c42525fbf96812d98d29077692bed425c
igour <1206@Gour>
2017-02-01 20:12:50 +0530

    checked-in code by Indrajeet

--- Indrajeet-quiz/src/main/java/hackerrank/tutorials/crackingtheinterview/Sorting_bubbleSort.java
+++ Indrajeet-quiz/src/main/java/hackerrank/tutorials/crackingtheinterview/Sorting_bubbleSort.java
@@ -1,44 +1,49 @@
 package hackerrank.tutorials.crackingtheinterview;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * Created by IGour on 1/30/2017.
  * problem url : https://www.hackerrank.com/challenges/ctci-bubble-sort
  */
 public class Sorting_bubbleSort {
-    public int i, j, temp = 0, numSwaps = 0;
+    public int i, j, temp = 0, numSwaps = 0, firstElement = 0, lastElement = 0;
 
-
-    public int[] bubbleSort(int[] list) {
-
+    public String[] bubbleSort(String[] list) {
         for (i = 0; i < list.length; i++) {
             for (j = 0; j < list.length - 1; j++) {
-
-                if (list[j] > list[j + 1]) {
-                    temp = list[j + 1];
+                if (j == 0) {
+                    firstElement = Integer.parseInt(list[j]);
+                }
+                if (Integer.parseInt(list[j]) > Integer.parseInt(list[j + 1])) {
+                    temp = Integer.parseInt(list[j + 1]);
                     list[j + 1] = list[j];
-                    list[j] = temp;
+                    list[j] = "" + temp;
+//                    list[j] = String.valueOf(temp) ;
                     numSwaps++;
                 }
-
+                lastElement = Integer.parseInt(list[j + 1]);
             }
         }
         return list;
     }
 
     public static void main(String[] args) {
-        Sorting_bubbleSort sbs = new Sorting_bubbleSort();
-        int[] ip = {5, 10, 1, 3, 2, 7, 9, 6};
-        ArrayList<Integer> al = new ArrayList<Integer>();
-        System.out.println("Old int array : " + Arrays.toString(ip));
-
-        sbs.bubbleSort(ip);
+        Scanner in = new Scanner(System.in);
+        Integer numOfElement = Integer.parseInt(in.nextLine());
+        String nextline = in.nextLine();
+        String[] values = nextline.split(",");
+//        int[] ip = {5, 10, 1, 3, 2, 7, 9, 6};
+//        ArrayList<Integer> al = new ArrayList<Integer>();
+//        System.out.println("Old int array : " + Arrays.toString(ip));
         // to print array element without iterate
-        System.out.println("New int array : " + Arrays.toString(ip));
-//        System.out.println("Array is sorted in " + numSwaps + " swaps.");
-        System.out.println("First Element: firstElement, where  is the first element in the sorted array.");
+//        System.out.println("New int array : " + Arrays.toString(ip));
 
+        Sorting_bubbleSort sbs = new Sorting_bubbleSort();
+        sbs.bubbleSort(values);
+//        desire ouput format
+        System.out.println("Array is sorted in " + sbs.numSwaps + " swaps.");
+        System.out.println("First Element: " + sbs.firstElement);
+        System.out.println("Last Element: " + sbs.lastElement);
     }
 }
